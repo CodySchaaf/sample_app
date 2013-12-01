c2cbcdde5051607b49d28228151c0cc6d5e9cbdb
Cody Schaaf <cody@csc8.com>
2013-12-01 01:01:45 -0800

    finished sign-in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
 	it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
 	it { should be_valid }
@@ -52,7 +53,7 @@ describe User do
   	end
 	end
 
-	describe "when amail address is already taken" do 
+	describe "when email address is already taken" do 
 		before do
 			user_with_same_email = @user.dup
 			user_with_same_email.email = @user.email.upcase
@@ -94,5 +95,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
