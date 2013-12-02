e95c1c76286909ed881523b84df9d286da763ab3
Cody Schaaf <cody@csc8.com>
2013-12-01 16:54:10 -0800

    somestuff

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,25 +41,30 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",		with: "Example User"
-        fill_in "Email",				with: "user@example.com"
-        fill_in "Password",			with: "foobar"
-        fill_in "Confirmation",	with: "foobar"
-      end
-
-      describe 'after saving the user' do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
-
-        it { should have_link('Sign out')	}
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+      let(:user) {FactoryGirl.build(:user)}
+      before { valid_signup(user)}
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:new_user) { User.find_by(email: user.email) }
+
+        context 'when new_user is submited' do
+          it 'should equal user' do
+          # subject {new_user}
+          # its(:name) { should eq(user.name) }
+            expect (new_user.name).to eq(user.name)
+          end
+        end
+
+        it { should have_link('Sign out')	}
+        it { should have_title(new_user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end
 end
